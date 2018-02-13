e86f7fd2fe18c2ee31f360f258947a5e494dc69f
Mario Bejarano <mario.bejarano@me.com>
2018-02-13 15:01:27 +0000

    Added support for D3 to perform data grouping from JSON fime

--- routes/ethereum.js
+++ routes/ethereum.js
@@ -1,41 +1,44 @@
-var express = require('express');
-var fs = require('fs');
-var cheerio = require('cheerio');
-var cheerioTbl = require('cheerio-tableparser');
-var request = require('request');
+var express = require('express')
+var cheerio = require('cheerio')
+var request = require('request')
 var tabletojson = require('tabletojson')
-var router = express.Router();
-
-
+var d3 = require('d3')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   /*  res.send('respond with a resource'); */
-  coin = 'ethereum';
-  url = 'https://coinmarketcap.com/currencies/' + coin + '/';
-  request(url, function(error, response, html) {
-
+  var coin = 'ethereum'
+  var url = 'https://coinmarketcap.com/currencies/' + coin + '/'
+  request(url, function (error, response, html) {
     if (!error) {
-      var $ = cheerio.load(html);
-      var $ = $('#markets-table').parent().html();
-      var tableasjson = tabletojson.convert($);
-      tableasjson = JSON.stringify(tableasjson[0]);
+      var $ = cheerio.load(html)
+      var tableHtml = $('#markets-table').parent().html()
+      var tableasjson = tabletojson.convert(tableHtml)
+      var market = tableasjson[0]
+      tableasjson = JSON.stringify(tableasjson[0])
+      var marketByPair = d3.nest()
+        .key(function (d) {
+          return d.Pair
+        })
+        .entries(market)
+      console.log(JSON.stringify(marketByPair[0].values))
+      debugger
       // console.log(tableasjson);
       //      debugger;
       //      console.log($);
-      var tbl = $;
     } else {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-      var data = error;
+      console.log('error:', error) // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+      // console.log('body:', body) // Print the HTML for the Google homepage.
+      var data = error
     }
 
     res.render('ethereum', {
       title: coin,
-      data: tbl,
-      jsontbl: tableasjson
-    });
-  });
-});
-module.exports = router;
+      data: tableHtml,
+      jsontbl: JSON.stringify(marketByPair)
+    })
+  })
+})
+module.exports = router
