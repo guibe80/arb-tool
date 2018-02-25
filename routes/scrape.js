5aa8ee3754cf9e80e1b727ab6ff1fb7af0f95d35
Mario Bejarano <mario.bejarano@me.com>
2018-02-25 15:07:10 +0000

    File updated to meet EsLint recommendations

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,47 +1,48 @@
-var express = require('express');
-var fs      = require('fs');
-var cheerio = require('cheerio');
-var request = require('request');
-var router  = express.Router();
+var express = require('express')
+var fs = require('fs')
+var cheerio = require('cheerio')
+var request = require('request')
+var router = express.Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   /*  res.send('respond with a resource'); */
-  url = 'http://www.imdb.com/title/tt0364725/';
+  var url = 'http://www.imdb.com/title/tt0364725/'
 
-  request(url, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-      debugger;
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $ = cheerio.load(html)
+      // debugger
 
-      var title, release, rating;
-      var json = { title: "", release : "", rating : ""};
+      var title, release, rating
+      var json = {title: '', release: '', rating: ' '}
 
-      $('.title_wrapper').filter(function(){
-          var data = $(this);
-          title = data.children().first().text().trim();
-          release = data.children().last().children().last().text().trim();
-          json.title = title;
-          json.release = release;
+      $('.title_wrapper').filter(function () {
+        var data = $(this)
+        title = data.children().first().text().trim()
+        release = data.children().last().children().last().text().trim()
+        json.title = title
+        json.release = release
+      })
 
-       })
-
-       $('.ratingValue').filter(function(){
-          var data = $(this);
-          rating = data.text().trim();
-          json.rating = rating;
-    })
-  }
+      $('.ratingValue').filter(function () {
+        var data = $(this)
+        rating = data.text().trim()
+        json.rating = rating
+      })
+    }
 
 //  res.render('scrape', {movie: "title"});
-  fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-      console.log('File succesfully written! - Check your directory for the output.json file');
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
+      if (err) {
+        console.log('There was an error while writting the file')
+      } else {
+        console.log('File succesfully written! - Check your directory for the output.json file')
+      }
+    })
+
+    res.render('scrape', { movie: json.title, released: json.release, rating: json.rating })
   })
+})
 
-
-  res.render('scrape', { movie : json.title, released : json.release, rating : json.rating });
-
-  })
-});
-
-module.exports = router;
+module.exports = router
