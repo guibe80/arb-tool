8153b06b7594cd30ce529e7b8e72d1983ba2e9a4
Mario Bejarano <mario.bejarano@me.com>
2018-02-25 15:04:52 +0000

    Updated to call the scrape library coinmarketcap.js and applying filters

--- routes/ethereum.js
+++ routes/ethereum.js
@@ -1,51 +1,38 @@
 var express = require('express')
-var cheerio = require('cheerio')
-var request = require('request')
-var numeral = require('numeral')
-var tabletojson = require('tabletojson')
-var d3 = require('d3')
+// var cheerio = require('cheerio')
+// var request = require('request')
+// var numeral = require('numeral')
+var coinmarket = require('../lib/coinmarket.js')
+// var tabletojson = require('tabletojson')
+// var d3 = require('d3')
 var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   /*  res.send('respond with a resource'); */
-  var coin = 'litecoin'
-  var url = 'https://coinmarketcap.com/currencies/' + coin + '/'
-  request(url, function (error, response, html) {
-    if (!error) {
-      /* Extract data from HTML, convert to JSON and group by Pair */
-      var $ = cheerio.load(html)
-      var htmlData = $('#markets-table').parent().html()
-      // console.log(htmlData)
-      var jsonMarket = tabletojson.convert(htmlData)[0]
-      for (var i = 0; i < jsonMarket.length; i++) {
-        jsonMarket[i].Price = numeral(jsonMarket[i].Price)._value
-        jsonMarket[i]['Volume (24h)'] = numeral(jsonMarket[i]['Volume (24h)'])._value
-        jsonMarket[i]['Volume (%)'] = numeral(jsonMarket[i]['Volume (%)'])._value
-        // debugger
-        // console.log(jsonMarket[i].Price, jsonMarket[i]['Volume (24h)'], jsonMarket[i]['Volume (%)'])
-      }
-      var marketByPair = d3.nest()
-        .key(function (d) {
-          return d.Pair
-        })
-        .entries(jsonMarket)
-      // console.log(JSON.stringify(marketByPair[0].values))
-      // console.log(tableasjson);
-      //      debugger;
-      //      console.log($);
-    } else {
-      console.log('error:', error) // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-      // console.log('body:', body) // Print the HTML for the Google homepage.
-      var coin = error
-    }
 
+  var JSONCoinData, table
+  var coin = 'litecoin'
+
+  var CoinData = coinmarket.getCoinData(coin)
+  CoinData.then(function (data) {
+    console.log('Initialized user details')
+    JSONCoinData = coinmarket.updatedData(data)
+    // JSONCoinData = coinmarket.volumeGreaterThan(JSONCoinData, 1000)
+    JSONCoinData = coinmarket.groupByPair(JSONCoinData)
+    JSONCoinData = coinmarket.removeSingleData(JSONCoinData, 1)
+
+    // Use user details from here
+    // console.log(JSONCoinData[0])
+    table = JSONCoinData
     res.render('ethereum', {
       title: coin,
-      data: htmlData,
-      jsontbl: JSON.stringify(marketByPair)
+      data: 'htmlData',
+      jsontbl: JSON.stringify(table)
     })
+  }, function (error) {
+    console.log(error)
   })
 })
+
 module.exports = router
